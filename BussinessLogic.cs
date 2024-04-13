649eae831749abb3531084bf812df5ab31a2493a
Leilamica <163433407+Leilamica@users.noreply.github.com>
2024-04-13 12:53:38 +0800

    Add files via upload

---
+++ BussinessLogic.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ClassLibrary6
+{
+    public class BusinessLogic
+    {
+        public static double CalculateTotalBill(double price)
+        {
+
+            return price;
+        }
+    }
+}
---
+++ DataLogic.cs
@@ -0,0 +1,32 @@
+﻿
+namespace ClassLibrary6
+{
+    public class DataLogic
+    {
+        public static double GetPrice(string menuItem)
+        {
+        
+            switch (menuItem.ToLower())
+            {
+                case "adobong manok":
+                    return 50.00;
+                case "tinola":
+                    return 50.00;
+                case "sinigang":
+                    return 60.00;
+                case "bicol express":
+                    return 60.00;
+                case "kaldereta":
+                    return 50.00;
+                case "bopis":
+                    return 50.00;
+                case "chopsuy":
+                    return 30.00;
+                case "rice":
+                    return 10.00;
+                default:
+                    return 0.00; // If item not found
+            }
+        }
+    }
+}
